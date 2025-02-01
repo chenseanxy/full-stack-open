1f947af59f3565b22cb390c7599cebf728d06b78
Chen Xinyang <me@chenxy.tech>
2025-02-01 10:32:52 +0200

    part1: courseinfo complete

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,25 +1,39 @@
+// import Header from "./Header"
+// import Content from "./Content"
+// import Total from "./Total"
+
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <h1>{course}</h1>
       <p>
-        {part1} {exercises1}
+        {parts[0].name} {parts[0].exercises}
       </p>
       <p>
-        {part2} {exercises2}
+        {parts[1].name} {parts[1].exercises}
       </p>
       <p>
-        {part3} {exercises3}
+        {parts[2].name} {parts[2].exercises}
       </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <p>
+        Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+      </p>
     </div>
   )
 }
